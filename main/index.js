58b22f78004a625a59c94bd671dc8551c7452204
Michael Hernandez <mhernandez@jibe.com>
2014-03-09 19:59:10 -0400

    Added more files

--- main/index.js
+++ main/index.js
@@ -1,13 +1,32 @@
 var express         = require('express');
 var app             = express();
 var fs              = require('fs');
-
+var trumpet         = require('trumpet');
+var format          = require('util').format;
 app.use('/assets', express.static('public'));
 
 
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
-    fs.createReadStream(__dirname+'/public/index.html').pipe(res);
+    var files = fs.readdirSync('./public/music');
+    console.log(files);
+    //distortedsoul+distortedsoul
+    var tr = trumpet();
+    tr.pipe(res);
+    var writable = tr.select('div#auto-music').createWriteStream();
+    files.forEach(function(v) {
+        var name = v.split('distortedsoul+distortedsoul')[1].split('.mp3')[0];
+        console.log(name);
+        writable.write(format(fs.readFileSync('./public/music.tmpl').toString('utf8'),'Distorted Soul - ' + name, v));
+    });
+    writable.end(null);
+    var readable = fs.createReadStream(__dirname+'/public/index.html');
+    readable.pipe(tr);
+    readable.on('end', function () {
+        console.log('reading done');
+        tr.end();
+        res.end();
+    })
 })
 
 app.get('/music', function (req, res) {
